a2256916f46447fe0a81c1f742a6d2bf1e83e452
Uni <uni@pcteam138.team-pb.de>
2025-04-03 15:21:23 +0200

    Noch ein print hinzugefügt

---
+++ Julian_Hello_World.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Julian_Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Again Hello World");
+    }
+}
